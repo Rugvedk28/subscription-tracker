b1e0685aa99a584abb4f09f52d567c96d5fe086e
rugvedk28 <rugvedrk1189@gmail.com>
2026-02-10 23:59:19 +0530

    added upstash and qstash configuration, added get subscription for user route, added export default for subscription model

--- config/env.js
+++ config/env.js
@@ -5,5 +5,6 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 // keep original destructuring style but provide safe defaults so PORT is never undefined
 export const { PORT, NODE_ENV, DB_URI,
     JWT_SECRET, JWT_EXPIRES_IN,
-    ARCJET_KEY, ARCJET_ENV
+    ARCJET_KEY, ARCJET_ENV,
+        QSTASH_URL, QSTASH_TOKEN, QSTASH_CURRENT_SIGNING_KEY, QSTASH_NEXT_SIGNING_KEY
  } = process.env;
